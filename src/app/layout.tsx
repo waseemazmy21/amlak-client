a5e1e63e5b4e5521fcd2cd78cf5c412cd33ae66e
waseem azmy <waseemazmy21@gmail.com>
2025-08-05 17:07:53 +0300

    feat(theme): add light/dark theme - Implemented schadcn theme using css variables - Implemented theme context for theme management - Added ThemeToggle component for theme switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthContext";
 import QueryClientWrapper from "@/context/QueryClient";
+import ThemeProvider from "@/context/themeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,11 +31,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
-        <QueryClientWrapper>
-          <AuthProvider>
-            {children}
-          </AuthProvider>
-        </QueryClientWrapper>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <QueryClientWrapper>
+            <AuthProvider>
+              {children}
+            </AuthProvider>
+          </QueryClientWrapper>
+        </ThemeProvider>
       </body>
     </html>
   );
